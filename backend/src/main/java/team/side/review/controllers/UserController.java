a6c2853884839fd5efea339fd4189a4a7fc814d0
masterKimDY <ehddbs1017@gmail.com>
2023-03-08 23:09:45 +0900

    feat: 스웨거 설정 추가

--- backend/src/main/java/team/side/review/controllers/UserController.java
+++ backend/src/main/java/team/side/review/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/login")
-public class LoginController {
+public class UserController {
 
 
 }
