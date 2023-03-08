49374d4eeb678f7394d0d6e38b3be39dce8e2293
chiman <rlaclghks123@naver.com>
2023-03-08 18:21:45 +0900

    Refactor: 상수 내용 변경

--- frontend/src/utils/JoinConstants.tsx
+++ frontend/src/utils/JoinConstants.tsx
@@ -10,9 +10,9 @@ export const SUCCESS_AUTHENTICATION_BUSINESSNUMBER_MESSAGE = '인증되었습니
 export const SHUTDOWN_BUSINESSNUMBER_MESSAGE = '휴/폐업자 입니다. 다른 사업자번호를 입력해주세요';
 export const INVALID_BUSINESSNUMBER_MESSAGE = '국세청에 등록되지 않은 사업자등록번호입니다. 다시 입력해주세요';
 
-export const EMAIL_REQUEST_MESSAGE = '이메일 형식으로 입력해주세요';
-export const NICKNAME_REQUEST_MESSAGE = 'nickname을 입력해주세요';
-export const PASSWORD_REQUEST_MESSAGE = 'password를 입력해주세요';
-export const PASSWORD_RE_REQUEST_MESSAGE = '다시한번 password를 입력해주세요';
+export const EMAIL_REQUEST_MESSAGE = 'Email 형식에 맞게 입력해주세요';
+export const NICKNAME_REQUEST_MESSAGE = 'Nickname을 입력해주세요';
+export const PASSWORD_REQUEST_MESSAGE = 'Password를 입력해주세요';
+export const PASSWORD_RE_REQUEST_MESSAGE = '다시한번 Password를 입력해주세요';
 export const PHONENUMBER_REQUEST_MESSAGE = "' - ' 없이 핸드폰 번호 11자리를 입력해주세요 Ex) 01012345678";
 export const BUSINESSNUMBER_REQUEST_MESSAGE = " ' - ' 없이 사업자번호 10자리를 입력해주세요";
