1948d9342cbcb28ab806df7030e85ade69ad1dbe
masterKimDY <ehddbs1017@gmail.com>
2023-03-10 01:24:15 +0900

    feat: 체험단 등록, 삭제 스웨거 애너테이션 작성

--- backend/src/main/java/team/side/review/controllers/CampaignController.java
+++ backend/src/main/java/team/side/review/controllers/CampaignController.java
@@ -1,5 +1,9 @@
 package team.side.review.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.side.review.models.dto.CampaignCreateRequestDto;
 import team.side.review.models.dto.CampaignCreateResponsetDto;
+import team.side.review.models.dto.ErrorResponseDto;
 import team.side.review.models.dto.ResponseDto;
 
 import javax.validation.Valid;
@@ -17,18 +22,28 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/campaign")
 @Slf4j
+@Api(value = "체험단 API", description = "체험단 조회, 등록, 삭제")
 public class CampaignController {
 
     @PostMapping
+    @ApiOperation(value = "등록 API", notes = "체험단 등록")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "클라이언트 잘못된 요청", response = ErrorResponseDto.class),
+    })
     public ResponseEntity<ResponseDto<CampaignCreateResponsetDto>> createCampaign(
             @RequestBody @Valid CampaignCreateRequestDto requestDTO) {
+
         // TODO: 캠페인 생성 로직 구현
+
         CampaignCreateResponsetDto responsetDto = new CampaignCreateResponsetDto(1L);
         return ResponseEntity.ok(ResponseDto.success(responsetDto));
     }
     @DeleteMapping("/{campaignId}")
+    @ApiOperation(value = "삭제 API", notes = "체험단 삭제")
     public ResponseEntity<ResponseDto<Long>> deleteCampaign(@PathVariable Long campaignId) {
+
         // TODO: campaignId에 해당하는 캠페인 삭제 로직 구현
+
         return ResponseEntity.ok(ResponseDto.success(campaignId));
     }
 }
--- backend/src/main/java/team/side/review/models/dto/ResponseDto.java
+++ backend/src/main/java/team/side/review/models/dto/ResponseDto.java
@@ -1,5 +1,7 @@
 package team.side.review.models.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,10 +10,17 @@ import org.springframework.http.HttpStatus;
 @Getter
 @ToString
 @EqualsAndHashCode
+@ApiModel(value = "응답 정보", description = "API 응답에 대한 정보")
 public class ResponseDto<T> {
-    private Integer statusCode;
-    private String message;
-    private T data;
+
+    @ApiModelProperty(value = "상태코드", required = true)
+    private final Integer statusCode;
+
+    @ApiModelProperty(value = "메시지", required = true)
+    private final String message;
+
+    @ApiModelProperty(value = "응답 data")
+    private final T data;
 
     public static <T> ResponseDto<T> success(T data) {
         return new ResponseDto<>(HttpStatus.OK.value(), "success", data);
