62c5afd69f18bc7aab45bd51b7ea273a0e8450df
chiman <rlaclghks123@naver.com>
2023-03-21 19:38:54 +0900

    Refactor: 약관동의 체크박스 리팩토링
    map 함수를 사용하여 반복되는 코드를 줄이도록 리팩토링

--- frontend/src/components/CheckBoxForAgreement.tsx
+++ frontend/src/components/CheckBoxForAgreement.tsx
@@ -1,48 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const AGGREMENT_BOX = [
+  { title: '목적', contents: '이용자 식별및 본인 여부 확인 닉네임' },
+  { title: '항목', contents: '닉네임, 비밀번호, 이메일주소' },
+  {
+    title: '보유 및 이용기간',
+    contents: '회원 탈퇴 즉시  또는 이용 목적 달성 즉시 파기',
+  },
+];
+
+const AGGREMENT_CHECKBOX = [
+  {
+    contents:
+      '캠페인 모집 및 추천, 설문조사, 이벤트 정보등의 마케팅 수신 이메일, 문자 및 카카오톡) 에 동의합니다. (선택)',
+  },
+
+  {
+    contents: '서비스 이용약관에 동의합니다.',
+    mark: '(필수) 내용보기',
+  },
+  {
+    contents: '본인은 만 14세 이상입니다.',
+    mark: '(필수) 내용보기',
+  },
+  {
+    contents: '개인정보 수집및 이용에 동의합니다.',
+    mark: '(필수) 내용보기',
+  },
+];
+
 const CheckBoxForAgreement = () => {
   return (
     <Wrapper>
-      <CheckBox>
-        <input type="checkbox" id="check_btn" />
-        <label htmlFor="check_btn">
-          <p>캠페인 모집 및 추천, 설문조사, 이벤트 정보등의 마케팅 수신</p>
-          <p>(이메일, 문자 및 카카오톡) 에 동의합니다. (선택)</p>
-        </label>
-      </CheckBox>
-      <CheckBox>
-        <input type="checkbox" id="check_btn2" />
-        <label htmlFor="check_btn2">
-          서비스 이용약관에 동의합니다. (필수) <Mark>내용보기 </Mark>
-        </label>
-      </CheckBox>
-      <CheckBox>
-        <input type="checkbox" id="check_btn3" />
-        <label htmlFor="check_btn3">
-          본인은 만 14세 이상입니다. (필수) <Mark>내용보기</Mark>
-        </label>
-      </CheckBox>
-      <CheckBox>
-        <input type="checkbox" id="check_btn4" />
-        <label htmlFor="check_btn4">
-          개인정보 수집및 이용에 동의합니다. (필수) <Mark>내용보기</Mark>
-        </label>
-      </CheckBox>
+      {AGGREMENT_CHECKBOX.map((item, i) => {
+        return (
+          <CheckBox key={i}>
+            <input type="checkbox" id={`check_btn${i}`} />
+            <label htmlFor={`check_btn${i}`}>
+              <div>
+                {item.contents} <Mark>{item.mark}</Mark>
+              </div>
+            </label>
+          </CheckBox>
+        );
+      })}
 
       <Table>
-        <Box>
-          <div>목적</div>
-          <div> 이용자 식별및 본인 여부 확인 닉네임</div>
-        </Box>
-        <Box>
-          <div>항목</div>
-          <div>닉네임, 비밀번호, 이메일주소</div>
-        </Box>
-        <Box>
-          <div>보유및 이용기간</div>
-          <div>회원 탈퇴 즉시 또는 이용 목적 달성 즉시 파기</div>
-        </Box>
+        {AGGREMENT_BOX.map((item, i) => {
+          return (
+            <Box key={i}>
+              <div>{item.title}</div>
+              <div>{item.contents}</div>
+            </Box>
+          );
+        })}
       </Table>
     </Wrapper>
   );
