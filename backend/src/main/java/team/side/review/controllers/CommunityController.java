ef77b3f8025e0b8ccc6e6cf3c3407e08c3e0e250
Bohyun-Jo <whqhgus12@gmail.com>
2023-03-29 21:35:07 +0900

    Feat: communityService 구현

--- backend/src/main/java/team/side/review/controllers/CommunityController.java
+++ backend/src/main/java/team/side/review/controllers/CommunityController.java
@@ -3,10 +3,12 @@ package team.side.review.controllers;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.side.review.models.dto.*;
+import team.side.review.services.CommunityService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +16,16 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/community")
+@RequiredArgsConstructor
 @Api(value = "커뮤니티 API")
 public class CommunityController {
+
+    private final CommunityService communityService;
 
     @GetMapping("/")
     @ApiOperation(value = "메인페이지 조회")
     public List<CommunityMainResponseDto> mainPage(){
-        // TODO : CommunityService 구현
-        List<CommunityMainResponseDto> list = new ArrayList<>();
+        List<CommunityMainResponseDto> list = communityService.mainPage();
         return list;
     }
 
@@ -29,8 +33,8 @@ public class CommunityController {
     @ApiOperation("커뮤니티 글 작성")
     public ResponseEntity<ResponseDto<CommunityEditResponseDto>> edit(@RequestBody CommunityEditRequestDto
                                                                                   communityEditRequestDto){
-        // TODO : CommunityService 구현
-        CommunityEditResponseDto communityEditResponseDto = new CommunityEditResponseDto("success");
+
+        CommunityEditResponseDto communityEditResponseDto = communityService.edit(communityEditRequestDto);
 
         return ResponseEntity.ok(ResponseDto.success(communityEditResponseDto));
     }
@@ -38,9 +42,7 @@ public class CommunityController {
     @GetMapping("/{communityId}")
     @ApiOperation(value = "커뮤니티 상세 페이지")
     public ResponseEntity<ResponseDto<CommunityDetailResponseDto>> detail(@PathVariable Long communityId){
-        //TODO : CommunityService 구현
-        CommunityDetailResponseDto communityDetailResponseDto =
-                new CommunityDetailResponseDto("example", "wow", "me", null);
+        CommunityDetailResponseDto communityDetailResponseDto = communityService.detail(communityId);
         return ResponseEntity.ok(ResponseDto.success(communityDetailResponseDto));
     }
 
