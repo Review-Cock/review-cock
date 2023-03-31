024c5186290ab0b45323830c0b45175c9917a033
chiman <rlaclghks123@naver.com>
2023-03-31 12:40:59 +0900

    Design: 회원가입 글씨체 변경 및 체크박스 내용, 글씨체 수정

--- frontend/src/pages/Join/index.styles.tsx
+++ frontend/src/pages/Join/index.styles.tsx
@@ -12,4 +12,10 @@ export const JoinBox = styled.div`
 export const Title = styled.div`
   text-align: center;
   font-size: 36px;
+  color: #222222;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: bold;
+  line-height: 40px;
+  font-family: Pretendard;
 `;
