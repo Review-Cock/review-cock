95ee55ae7592b3847d7cf5f3de5f4e31b9b6e6f7
이도훈 <qwer2854@naver.com>
2023-04-03 18:25:09 +0900

    Feat: 이미지 base64로 인코딩

--- frontend/src/components/Register/ImageUpload/index.tsx
+++ frontend/src/components/Register/ImageUpload/index.tsx
@@ -2,13 +2,13 @@ import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 interface ImageUploadProps {
+  setImage: (data: any) => void;
   type: string;
 }
 
-const ImageUpload = ({ type }: ImageUploadProps) => {
+const ImageUpload = ({ type, setImage }: ImageUploadProps) => {
   const [isEmpty, setIsEmpty] = useState(true);
-  const [file, setFile] = useState<FileList>();
-  const [fileArray, setFileArray] = useState([]);
+  const [Base64s, setBase64s] = useState<{ image: File; url: any }[]>([]);
 
   const fileInputType = type === 'multiple' ? true : false;
 
@@ -20,19 +20,41 @@ const ImageUpload = ({ type }: ImageUploadProps) => {
 
   const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.currentTarget.files;
-    const fileArr = Array.from(files);
+    // 최대 등록갯수 제한
+    if (type === 'single' && Base64s.length > 0) return;
+    if (type === 'multiple' && Base64s.length > 3) return;
+
+    if (files.length > 4) {
+      window.alert('사진은 최대 4장까지 등록가능합니다.');
+      return;
+    }
     if (files.length === 0) {
       setIsEmpty(true);
       return;
     }
-    setFile(files);
-    setFileArray(fileArr);
     setIsEmpty(false);
+    // files가 바뀔때 files Array를 돌면서 base64로 인코딩 후 Base64s에 저장
+    Array.from(files).forEach((image) => {
+      encodeFileToBase64(image).then((data) => {
+        setBase64s((preV) => [...preV, { image: image, url: data }]);
+        setImage((preV: any) => [...preV, data]);
+      });
+    });
+  };
+
+  // base64로 인코딩
+  const encodeFileToBase64 = (image: File) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = (event: any) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+    });
   };
 
   return (
     <div>
-      <FileInput multiple={fileInputType} ref={fileRef} onChange={handleChangeFile} type="file" />
+      <FileInput multiple={fileInputType} ref={fileRef} onChange={handleChangeFile} accept="image/*" type="file" />
       {isEmpty && (
         <ImageBox>
           <p>다운이미지</p>
@@ -45,8 +67,8 @@ const ImageUpload = ({ type }: ImageUploadProps) => {
 
       {!isEmpty && (
         <ImageBox>
-          {fileArray.map((v, i) => (
-            <div key={i}>{v.name}</div>
+          {Base64s.map((v, i) => (
+            <div key={i}>{v.image.name}</div>
           ))}
           <p>
             <span onClick={handleOpenFinder}>여기</span>를 클릭하여 등록하세요.
