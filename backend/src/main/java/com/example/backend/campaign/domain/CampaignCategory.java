d4d6e5163b6650424d397cdcf98ddbbb430b0c82
jiseunghyeon <3aroa2@gmail.com>
2023-04-20 00:16:23 +0900

    Feat: Campaign 카테고리 양방향 설정 [#2]

--- backend/src/main/java/com/example/backend/campaign/domain/CampaignCategory.java
+++ backend/src/main/java/com/example/backend/campaign/domain/CampaignCategory.java
@@ -1,9 +1,14 @@
 package com.example.backend.campaign.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class CampaignCategory {
@@ -13,4 +18,7 @@ public class CampaignCategory {
     private Long id;
 
     private String title;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Campaign> campaigns = new ArrayList<>();
 }
