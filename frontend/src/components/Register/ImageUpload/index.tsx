8ef419dc72b03da8b9ea124a931844435dc87a44
이도훈 <qwer2854@naver.com>
2023-04-21 23:55:24 +0900

    Feat: Drag & Drop 구현 [#10]

--- frontend/src/components/Register/ImageUpload/index.tsx
+++ frontend/src/components/Register/ImageUpload/index.tsx
@@ -15,12 +15,17 @@ const ImageUpload = ({ type, setImage }: ImageUploadProps) => {
   const fileInputType = type === 'multiple' ? true : false;
   const fileRef = useRef(null);
 
-  const handleOpenFinder = () => {
-    fileRef.current.click();
-  };
+  const changeFile = (files: FileList) => {
+    // 드래그 시 파일유형 검사
+    const fileForm = /(.*?)\.(jpg|jpeg|png|gif|bmp|pdf)$/;
+    for (const file of Array.from(files)) {
+      if (!file.name.match(fileForm)) {
+        alert('이미지 파일만 업로드 가능합니다.');
+        return;
+      }
+    }
 
-  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.currentTarget.files;
+    // 사진갯수 검사
     if (files.length === 0) return;
     if (!fileInputType && preImageList.length + files.length > 1) {
       alert('대표이미지는 1장만 등록가능합니다.');
@@ -30,13 +35,16 @@ const ImageUpload = ({ type, setImage }: ImageUploadProps) => {
       alert('파일은 최대 4장까지 등록가능합니다.');
       return;
     }
-    let urlList: string[] = [];
 
+    let urlList: string[] = [];
+    // 첫 사진 등록
     if (preImageList.length === 0) {
       setFileList(files);
       setImage(files);
       urlList = Array.from(files).map((v) => URL.createObjectURL(v));
-    } else {
+    }
+    // 사진이 이미 있을 때
+    else {
       const dataTransfer = new DataTransfer();
 
       Array.from(fileList).forEach((file) => dataTransfer.items.add(file));
@@ -50,6 +58,14 @@ const ImageUpload = ({ type, setImage }: ImageUploadProps) => {
 
     setPreImageList(urlList);
     setIsEmpty(false);
+  };
+
+  const handleOpenFinder = () => {
+    fileRef.current.click();
+  };
+
+  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeFile(e.currentTarget.files);
   };
 
   const handleRemoveBtn = (index: number) => {
@@ -69,8 +85,17 @@ const ImageUpload = ({ type, setImage }: ImageUploadProps) => {
     }
   };
 
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleOnDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    changeFile(e.dataTransfer.files);
+  };
+
   return (
-    <div>
+    <div onDrop={handleOnDrop} onDragOver={handleDragOver}>
       <FileInput multiple={fileInputType} ref={fileRef} onChange={handleChangeFile} accept="image/*" type="file" />
       {isEmpty && (
         <ImageBox>
