1bfb6fb793eba9afeed741f6f24b3f144542883a
chxxyx <codud0826@naver.com>
2023-04-28 22:07:14 +0900

    Feat: Security Config JWTAuthenticateFilter 적용 [#4]

--- backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/security/SecurityConfig.java
@@ -1,30 +1,36 @@
 package com.example.backend.config.security;
 
+import com.example.backend.config.security.jwt.filter.JwtAuthenticateFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+	private final JwtAuthenticateFilter authenticationFilter;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().disable()
-			.cors().disable()
-			.csrf().disable()
-			.headers().frameOptions().disable();
-			http
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.authorizeHttpRequests()
-				.requestMatchers("/**").permitAll();
+		http.httpBasic().disable().cors().disable().csrf().disable();
+
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.authorizeHttpRequests().requestMatchers("/**").permitAll().and()
+			.addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
