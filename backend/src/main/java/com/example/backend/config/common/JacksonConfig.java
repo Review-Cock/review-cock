1e1354fde74b39d02f16b25fd97bb83b78975bf4
jiseunghyeon <3aroa2@gmail.com>
2023-04-30 15:42:15 +0900

    Feat: Jackson 커스터마이징 [#2]

---
+++ backend/src/main/java/com/example/backend/config/common/JacksonConfig.java
@@ -0,0 +1,28 @@
+package com.example.backend.config.common;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+@Configuration
+public class JacksonConfig {
+
+    private static final String UTC = "UTC";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.timeZone(TimeZone.getTimeZone(UTC));
+            jacksonObjectMapperBuilder.simpleDateFormat(DATETIME_FORMAT);
+            jacksonObjectMapperBuilder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+            jacksonObjectMapperBuilder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
+        };
+    }
+}
