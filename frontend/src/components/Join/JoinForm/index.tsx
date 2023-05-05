0ed50779a97b300ff80179bb32fcf13b4292531a
chiman <rlaclghks123@naver.com>
2023-05-06 01:10:13 +0900

    Fix: 회원가입 유효성 검사 수정

--- frontend/src/components/Join/JoinForm/index.tsx
+++ frontend/src/components/Join/JoinForm/index.tsx
@@ -18,7 +18,7 @@ interface IForm {
   email: string;
   nickname: string;
   password: string;
-  passwordConfirm: string;
+  passwordCheck: string;
   phoneNumber: number;
 }
 
@@ -34,12 +34,12 @@ const JoinForm = () => {
   } = useForm<IForm>();
 
   const JoinMutation = useMutation(
-    ({ email, nickname, password, passwordConfirm, phoneNumber }: IForm) =>
+    ({ email, nickname, password, passwordCheck, phoneNumber }: IForm) =>
       axios.post('', {
         email,
         nickname,
         password,
-        passwordConfirm,
+        passwordCheck,
         phoneNumber,
       }),
     {
@@ -54,18 +54,18 @@ const JoinForm = () => {
   );
 
   const onValid = (data: IForm) => {
-    const { email, nickname, password, passwordConfirm, phoneNumber } = data;
+    const { email, nickname, password, passwordCheck, phoneNumber } = data;
 
-    if (password !== passwordConfirm) {
-      setError('passwordConfirm', { message: NOT_CORRECT_PASSWORD_MESSAGE }, { shouldFocus: true });
+    if (password !== passwordCheck) {
+      setError('passwordCheck', { message: NOT_CORRECT_PASSWORD_MESSAGE }, { shouldFocus: true });
       setValue('password', '');
-      setValue('passwordConfirm', '');
+      setValue('passwordCheck', '');
     } else {
       JoinMutation.mutate({
         email,
         nickname,
         password,
-        passwordConfirm,
+        passwordCheck,
         phoneNumber,
       });
     }
@@ -81,7 +81,7 @@ const JoinForm = () => {
         {...register('email', {
           required: EMAIL_REQUEST_MESSAGE,
           pattern: {
-            value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
+            value: /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z.]{2,6}$/,
             message: EMAIL_REQUEST_MESSAGE,
           },
         })}
@@ -89,12 +89,19 @@ const JoinForm = () => {
         type="email"
       />
       <ErrorBox>{errors.email?.message}</ErrorBox>
+
       <Label htmlFor="nickname">
         닉네임 <RedStar>*</RedStar>
       </Label>
       <JoinInput
         id="nickname"
-        {...register('nickname', { required: NICKNAME_REQUEST_MESSAGE })}
+        {...register('nickname', {
+          required: NICKNAME_REQUEST_MESSAGE,
+          pattern: {
+            value: /^[a-zA-Z0-9]{2,10}$/,
+            message: '영문, 숫자가 혼합된 2에서 10자리의 닉네임을 입력해주세요',
+          },
+        })}
         placeholder={NICKNAME_REQUEST_MESSAGE}
       />
       <ErrorBox>{errors?.nickname?.message}</ErrorBox>
@@ -107,7 +114,7 @@ const JoinForm = () => {
         {...register('password', {
           required: PASSWORD_REQUEST_MESSAGE,
           pattern: {
-            value: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+~|<>?]).{8,20}$/,
+            value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/,
             message: '영문, 숫자, 특수문자가 혼합된 8에서 20자리의 비밀번호를 입력해주세요',
           },
         })}
@@ -115,22 +122,22 @@ const JoinForm = () => {
         type="password"
       />
       <ErrorBox>{errors?.password?.message}</ErrorBox>
-      <Label htmlFor="passwordConfirm">
+      <Label htmlFor="passwordCheck">
         비밀번호 확인 <RedStar>*</RedStar>
       </Label>
       <JoinInput
-        id="passwordConfirm"
-        {...register('passwordConfirm', {
+        id="passwordCheck"
+        {...register('passwordCheck', {
           required: PASSWORD_RE_REQUEST_MESSAGE,
           pattern: {
-            value: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+~|<>?]).{8,20}$/,
+            value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/,
             message: '영문, 숫자, 특수문자가 혼합된 8에서 20자리의 비밀번호를 입력해주세요',
           },
         })}
         placeholder={PASSWORD_RE_REQUEST_MESSAGE}
         type="password"
       />
-      <ErrorBox>{errors?.passwordConfirm?.message}</ErrorBox>
+      <ErrorBox>{errors?.passwordCheck?.message}</ErrorBox>
 
       <Label htmlFor="phoneNumber">
         휴대전화 <RedStar>*</RedStar>
@@ -140,7 +147,7 @@ const JoinForm = () => {
         {...register('phoneNumber', {
           required: PHONENUMBER_REQUEST_MESSAGE,
           pattern: {
-            value: /\d{3}\d{4}\d{4}$/,
+            value: /^\d{2,3}\d{3,4}\d{4}$/,
             message: PHONENUMBER_REQUEST_MESSAGE,
           },
           maxLength: { value: 11, message: PHONENUMBER_REQUEST_MESSAGE },
