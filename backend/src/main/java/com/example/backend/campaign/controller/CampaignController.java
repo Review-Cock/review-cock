70c2b17bbf9bc9264ad72e0237d88157f32c93c2
jiseunghyeon <3aroa2@gmail.com>
2023-05-16 21:15:17 +0900

    Fix: 캠페인 상세 조회 API 변경

--- backend/src/main/java/com/example/backend/campaign/controller/CampaignController.java
+++ backend/src/main/java/com/example/backend/campaign/controller/CampaignController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -63,9 +64,8 @@ public class CampaignController {
     }
 
     @Operation(summary = "캠페인 상세 조회")
-    @Authenticated
-    @GetMapping("/detail/{no}")
-    public ResponseEntity<CampaignResponse> detail(@PathVariable String no) {
+    @GetMapping("/detail")
+    public ResponseEntity<CampaignResponse> detail(@RequestParam String no) {
         CampaignResponse response = campaignService.detail(no);
 
         return ResponseEntity.ok(response);
