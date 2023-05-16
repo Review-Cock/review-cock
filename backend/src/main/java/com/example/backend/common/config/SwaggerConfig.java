b46c18947eb423f65ae68b53ab36146dab11138b
jiseunghyeon <3aroa2@gmail.com>
2023-05-16 10:04:08 +0900

    Feat: 캠페인 참가 [#2]

--- backend/src/main/java/com/example/backend/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/example/backend/common/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -27,6 +28,6 @@ public class SwaggerConfig {
                         .bearerFormat("JWT"))
             )
             .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
-            .info(new Info().title("serviceTitle").version("serviceVersion"));
+            .info(new Info().title("리뷰콕 API Docs").version("v1"));
     }
 }
