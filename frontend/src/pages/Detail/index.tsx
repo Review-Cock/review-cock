fe0b10ae753d004905c74a869b1e2dbcd536dfc9
이도훈 <qwer2854@naver.com>
2023-05-17 20:55:14 +0900

    Fix: 신청마감일 참가버튼 비활성화 문제 수정 [#52]

--- frontend/src/pages/Detail/index.tsx
+++ frontend/src/pages/Detail/index.tsx
@@ -248,7 +248,7 @@ const Detail = () => {
           </TimeInfoWrapper>
           <Calendar dates={dates} />
           <SubmitBox>
-            {calDay > 0 && isLogin && (
+            {calDay >= 0 && isLogin && (
               <div>
                 <SnsLinkText htmlFor="snsLink">나의 SNS 링크</SnsLinkText>
                 <input value={userSnsLink} onChange={onChangeUserSnsLink} type="text" id="snsLink" />
